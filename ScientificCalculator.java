d7b66d0394509674e3ab289df6adf80fc4ce6c7d
Princess Imelda Odato <77333883+122839@users.noreply.github.com>
2024-02-06 17:00:48 +0300

    ScientificCalculator.java

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -7,22 +7,31 @@ public class ScientificCalculator extends JFrame implements ActionListener {
     //the frame
     static JFrame calc;
     //the text field
-    static JTextField txtfld;
+    static JTextField calcfld;
+    //for large number inputs with commas
     private DecimalFormat decf = new DecimalFormat("#,###.00");
+    //zero constant on the calculating panel
+    private int operator = 0;
+    private JTextArea screen=new JTextArea(5,40);
+    private JPanel panel = new JPanel(new BorderLayout(5,5));
 
     public ScientificCalculator(){
         calc = new JFrame("My Scientific Calculator");
-        calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calc.setSize(850,450);
-        txtfld = new JTextField();
-        txtfld.setFont(new Font("Times New Roman", Font.PLAIN,20));
-        txtfld.setEditable(false);
-        txtfld.setSize(425,70);
-        calc.add(txtfld , BorderLayout.NORTH);
+        calcfld = new JTextField();
+        calcfld.setFont(new Font("Times New Roman", Font.PLAIN,20));
+        calcfld.setEditable(false);
+        calcfld.setSize(425,70);
+        calc.add(calcfld , BorderLayout.NORTH);
+
+        screen.setBackground(Color.DARK_GRAY);
+        screen.setForeground(Color.WHITE);
+        panel.setBackground(Color.WHITE);
 
         JPanel bttnpanel = new JPanel();
-        bttnpanel.setLayout(new GridLayout(5,10));
+        bttnpanel.setLayout(new GridLayout(5,10,3,2));
 
+        //array with all the symbols the calculator needs
         String[] buttons = {
                 "(", ")","mc","m+","m-","mr","C","+/-","%","÷",
                 "2nd", "x²","x³","Xy", "e^", "10x", "7", "8","9", "x",
@@ -35,24 +44,28 @@ public class ScientificCalculator extends JFrame implements ActionListener {
         for(String button:buttons){
             JButton bttn = new JButton(button);
             bttn.setOpaque(false);
-            bttn.setBorderPainted(false);
-            bttn.setForeground(Color.MAGENTA);
-            bttn.setBackground(Color.MAGENTA);
+            bttn.setBorderPainted(true);
+            bttn.setForeground(Color.BLACK);
+            bttn.setBackground(Color.darkGray);
             bttn.setFont(new Font("Times New Roman",Font.PLAIN,18));
-            bttn.addActionListener(this);
+            bttn.addActionListener(this);  //attaching action listener to each button
             bttnpanel.add(bttn);
         }
 
-        calc.add(bttnpanel, BorderLayout.CENTER);
+        bttnpanel.setBackground(Color.DARK_GRAY);
+        panel.add(screen, BorderLayout.NORTH); 
+        panel.add(bttnpanel, BorderLayout.CENTER);
+        calc.add(panel);
+        calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calc.setVisible(true);
 
     }
-
 
     public static void main (String args[]){
         new ScientificCalculator();
     }
 
+    //overriding the method for action listener
     @Override
     public void actionPerformed(ActionEvent e) {
 
